6d24bf8b0aea606cd9261afb9708f531f1791dd5
EduardoVega04 <60398965+EduardoVega04@users.noreply.github.com>
2020-12-10 23:58:07 -0500

    Fix solution

--- 0x00-lockboxes/0-lockboxes.py
+++ 0x00-lockboxes/0-lockboxes.py
@@ -1,9 +1,18 @@
 #!/usr/bin/python3
-"""Lockboxes, an interview coding problem.
-   Determine if all boxes can be opened"""
+""" Lockboxes """
 
 
 def canUnlockAll(boxes):
-    """Function that evaluates if all boxes can be opened"""
-    def canUnlockAll(boxes):
-        
+    """ determines if all the boxes can be opened """
+
+    found_keys = []
+    for i in range(len(boxes)):
+        for j in range(len(boxes[i])):
+            if i < boxes[i][j] < len(boxes):
+                found_keys.append(boxes[i][j])
+        if len(boxes[i]) == 0 and i == len(boxes) - 1:
+            found_keys.append(0)
+    if len(found_keys) == len(boxes):
+        return True
+    else:
+        return False
