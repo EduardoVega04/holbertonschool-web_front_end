84daca30992bf00979c57734bdb775c79a76ed0a
EduardoVega04 <eduarvega04@hotmail.com>
2021-02-21 02:23:47 -0500

    Execution stack & timing execution

---
+++ 0x0C-Javascript_advanced/10-prime.js
@@ -0,0 +1,31 @@
+function countPrimeNumbers() {
+    let numberOfPrimes = 1;
+    let isPrime = 1;
+
+    for(let i = 2; i <= 100; i++)
+    {
+        if (i % 2 == 0)
+            continue;
+            
+        isPrime = 1;
+
+        for(let div = 2; div < i; div++)
+        {
+            if(i % div == 0)
+                isPrime = 0;
+        }
+        
+        if(isPrime)
+            numberOfPrimes++;
+    }
+    
+    return numberOfPrimes;
+}
+
+const startTime = performance.now();
+
+for (let i = 0; i < 100; i++)
+    countPrimeNumbers();
+
+const endTime = performance.now();
+console.log(`Execution time of calculating prime numbers 100 times was ${endTime - startTime} milliseconds.`);
